1c40bd7984184c09b25c822ea199b8008086c020
zhangliqing <623009470@qq.com>
2017-11-03 16:59:24 +0800

    1103

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,8 +1,8 @@
 (function() {
     'use strict';
-    angular.module('phoenix').value('reqUrl', "http://localhost:8080")
+    //angular.module('phoenix').value('reqUrl', "http://localhost:8080")
 
-    // angular.module('phoenix').value('reqUrl', "http://www.x-lab.ac:13001")
+     angular.module('phoenix').value('reqUrl', "http://www.x-lab.ac:13001")
 
     //toastr配置
     toastr.options = {
