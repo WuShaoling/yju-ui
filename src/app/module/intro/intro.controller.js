90f65da20672d99caba8b6f1f23ca122c2efce1e
juny1zhang <juny12324@gmail.com>
2017-11-07 10:31:32 +0800

    fix bug

--- src/app/module/intro/intro.controller.js
+++ src/app/module/intro/intro.controller.js
@@ -9,13 +9,13 @@
 
     function introCtrl($timeout) {
 
-        $('.imgctrl').height($(window).height() - 72)
+        $('.imgctrl').height($(window).height())
 
         $(window).bind('resize', function() {
             console.log(123);
             $timeout(function() {
 
-                $('.imgctrl').height($(window).height() - 72)
+                $('.imgctrl').height($(window).height())
 
             }, 500)
         });
