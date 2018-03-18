a18f128d775bbf8b035d86744860d7c21c0a7636
wen.shi <elogo1324@live.com>
2018-03-18 12:57:55 +0800

    Release

--- autoCI.sh
+++ autoCI.sh
@@ -1,27 +1,32 @@
 #! /bin/bash
 
-apt-get update
-echo "install git..."
-apt-get install -y git
-
-echo "installing npm..."
-apt-get install -y npm
-ln -s /usr/bin/nodejs /usr/bin/node
-
-echo "install bower..."
-npm install -g bower
-
-echo "install gulp..."
-npm install -g gulp
-
-echo "npm installing..."
-npm install
-
-echo "bower installing..."
-bower install --allow-root
-
+#apt-get update
+#echo "install git..."
+#apt-get install -y git
+#
+#echo "installing npm..."
+#apt-get install -y npm
+#ln -s /usr/bin/nodejs /usr/bin/node
+#
+#echo "install bower..."
+#npm install -g bower
+#
+#echo "install gulp..."
+#npm install -g gulp
+#
+#echo "npm installing..."
+#npm install
+#
+#echo "bower installing..."
+#bower install --allow-root
+#
 echo "Cleaning..."
 gulp clean
 
 echo "Building..."
 gulp build
+
+echo "Releasing..."
+git add .
+git ci -am "Release"
+git push
