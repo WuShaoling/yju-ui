704859f3d0c6f933d42947f5028b70f0fc1bc0cc
wen.shi <elogo1324@live.com>
2018-03-18 13:56:53 +0800

    Revert "Release"
    This reverts commit 07762a66bee9d98e87ba529e86c6e56882b1317d.

--- src/app/module/cloudware/cloudware.controller.js
+++ src/app/module/cloudware/cloudware.controller.js
@@ -320,7 +320,7 @@
                     var buf = new ArrayBuffer(5);
                     var dv = new DataView(buf);
                     dv.setUint8(0, 3);
-                    dv.setUint32(1, mapKey(i), true);
+                    dv.setUint32(1, mapKey(e.which), true);
                     if (ws.readyState == 1 && canvasOnFocus)
                         ws.send(buf);
                 };
