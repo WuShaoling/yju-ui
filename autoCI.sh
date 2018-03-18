96018fe49bebad44d32cadd4fa6a675d3e264ca3
wen.shi <elogo1324@live.com>
2018-03-18 11:23:41 +0800

    fix build error

--- autoCI.sh
+++ autoCI.sh
@@ -5,12 +5,6 @@ apt-get update
 apt-get install -y npm
 ln -s /usr/bin/nodejs /usr/bin/node
 
-echo "install bower..."
-npm install -g bower
-
-echo "install gulp..."
-npm install -g gulp
-
 echo "npm installing..."
 npm install
 
